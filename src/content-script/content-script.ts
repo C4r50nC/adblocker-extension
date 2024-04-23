60bfb35f20cd46f0eff4303bcd527f8c0084629b
C4r50nC <xichen059@gmail.com>
2024-04-23 18:46:24 +1000

    feat: implement 2 types of advertisement content removal

--- src/content-script/content-script.ts
+++ src/content-script/content-script.ts
@@ -1,3 +1,25 @@
-chrome.runtime.sendMessage("From the content script", (response) => {
-  console.log(response);
-});
+const rules: {
+  [url: string]: () => void;
+} = {
+  "https://www.nytimes.com/section/technology": filterNYTTechnologyByClass,
+};
+
+function filterNYTTechnology() {
+  const siteContent = document.getElementById("site-content");
+  const topWrapper = document.getElementById("top-wrapper");
+  siteContent.removeChild(topWrapper);
+}
+
+function filterNYTTechnologyByClass() {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    if (div.className.indexOf("ad ") !== -1) {
+      div.style.display = "none";
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log(document.URL);
+  rules[document.URL]();
+}
